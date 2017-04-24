a1c45939bcb6bc1987ff564cd6767d33fb891f37
willispyoung <willispyoung@gmail.com>
2017-04-24 14:06:03 +0800

    add control socket

--- Client.py
+++ Client.py
@@ -7,11 +7,17 @@ from Modifier import *
 server_address = (sys.argv[1], eval(sys.argv[2]))
 key_map = load_map("map.txt")
 
-
 def get_server_socket():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect(server_address)
     return server
+
+
+def add_listen_port(port):
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.bind(("", port))
+    listen_socket.listen(20)
+    return listen_socket
 
 
 def read_user(user, server):
@@ -44,15 +50,32 @@ def handle_user(user):
     threading.Thread(target=read_server, args=(user, server)).start()
 
 
-a_user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-a_user.bind(("", 22222))
-a_user.listen(20)
+control_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+control_socket.bind((""), 22222)
+control_socket.listen(20)
 
-listen_list = [a_user]
+# a_user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# a_user.bind(("", 22222))
+# a_user.listen(20)
+
+listen_list = [control_socket]
 
 while True:
     read_list, write_list, err_list = select.select(listen_list, [], [])
 
     for req in read_list:
-        (user, address) = req.accept()
-        handle_user(user)
+        if req == control_socket:
+            msg = req.recv(256)
+            # command: add#12345 --> listen on port 12345
+            command = msg.split('#')
+            if command[0] == "add":
+                port = 0
+                try:
+                    port = eval(command[1])
+                    listen_socket = add_listen_port(port)
+                    listen_list.append(listen_socket)
+                except socket.error:
+                    print("add port", port, "failed")
+        else:
+            (user, address) = req.accept()
+            handle_user(user)
