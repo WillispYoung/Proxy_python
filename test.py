cee1b3dfda4b7a33629ae63bf7add55f5fb2dcc5
willispyoung <willispyoung@gmail.com>
2017-05-14 11:12:55 +0800

    modify test

--- test.py
+++ test.py
@@ -4,21 +4,30 @@ import time
 
 data = json.load(open("init/config.json"))
 
-msgs = ["addport@13579,2",
-        "upgrade@13579,3",
-        "getflow@13579",
-        "preflow@13579",
-        "getIP@13579",
-        "getIPList@13579",
-        "close@13579,3",
+msg1 = ["addport@13579,2",
+        "upgrade@13579,3"]
+
+msg2 = ["getflow@12345",
+        "preflow@12345",
+        "getIP@12345",
+        "getIPList@12345"]
+
+msg3 = ["close@13579,3",
         "reopen@13579"]
 
-for i in range(len(msgs)):
+
+for m in msg1:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((data["client_ip"], int(data["control_socket_port"])))
-    s.send(bytearray(msgs[i], encoding="utf-8"))
-    if 1 < i < 6:
-        msg = s.recv(4096).decode("utf-8")
-        print(msg)
+    s.send(bytearray(m, encoding="utf-8"))
+    s.close()
+    time.sleep(1)
+
+for m in msg2:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((data["client_ip"], int(data["control_socket_port"])))
+    s.send(bytearray(m, encoding="utf-8"))
+    msg = s.recv(1024)
+    print(msg)
     s.close()
     time.sleep(1)
