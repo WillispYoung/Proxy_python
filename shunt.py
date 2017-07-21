497df5a1754a17f73dea72d399562352628a81af
willispyoung <willispyoung@gmail.com>
2017-07-21 15:47:30 +0800

    phase 0

--- shunt.py
+++ shunt.py
@@ -1,6 +1,7 @@
 import json
 import socket
 import re
+import time
 import subprocess
 from threading import Thread
 from modifier import *
@@ -10,12 +11,10 @@ class Client(object):
     def __init__(self):
         try:
             data = json.load(open("init/config.json"))
-            self.local_proxy_addr = ("", data["shunt"]["local_proxy_port"])
-            self.listen_addr = ("", data["shunt"]["listen_port"])
+            # use ("localhost", port) not ("", port)
+            self.local_proxy_addr = ("localhost", data["shunt"]["local_proxy_port"])
+            self.listen_addr = ("localhost", data["shunt"]["listen_port"])
             self.remote_proxy_addr = (data["shunt"]["remote_proxy_ip"], data["shunt"]["remote_proxy_port"])
-            print("listening:", self.listen_addr)
-            print("local proxy:", self.local_proxy_addr)
-            print("remote proxy:", self.remote_proxy_addr)
         except IOError:
             print("config file not found")
             exit(1)
@@ -25,6 +24,7 @@ class Client(object):
         self.acceptor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.acceptor.bind(self.listen_addr)
         self.acceptor.listen(20)
+        print("shunt program listening on port", self.listen_addr[1])
 
         self.user_proxy_dict = {}       # means user socket and proxy socket pair
         self.user_proxy_status = {}     # means using VPN or not
@@ -56,34 +56,35 @@ class Client(object):
                         if self.check_type(first_line):
                             print(first_line)
                             proxy = self.get_local_proxy()
+                            proxy.settimeout(10)
                             self.user_proxy_dict[u] = proxy
                             self.user_proxy_status[u] = "noVPN"
+                            Thread(target=self.proxy2user, args=(user, proxy)).start()
                     except UnicodeDecodeError:
                         pass
                 proxy.send(msg)
             except socket.error:
                 break
-        user.close()
-        proxy.close()
 
     def proxy2user(self, u, p):
         user = u
         proxy = p
         while True:
             if self.user_proxy_dict[u] != proxy:
-                proxy = self.user_proxy_dict[u]
+                proxy.close()
+                break
             try:
                 msg = proxy.recv(4096)
                 user.send(msg)
             except socket.error:
                 break
-        user.close()
-        proxy.close()
 
     def handle_user_connection(self, s):
         rps = self.get_remote_proxy()
         self.user_proxy_dict[s] = rps
         self.user_proxy_status[s] = "VPN"
+        s.settimeout(10)
+        rps.settimeout(10)
         Thread(target=self.user2proxy, args=(s, rps)).start()
         Thread(target=self.proxy2user, args=(s, rps)).start()
 
@@ -95,6 +96,7 @@ class Client(object):
 if __name__ == "__main__":
     try:
         subprocess.Popen("C:/squid/sbin/squid", shell=True)
+        print("local squid started")
     finally:
         pass
     c = Client()
